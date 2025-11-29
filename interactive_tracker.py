8c0773ed8f3d41b04913cc7e245ab15ce23e2726
dycanee <kendycane13@gmail.com>
2025-11-29 15:25:45 +0800

    test

--- interactive_tracker.py
+++ interactive_tracker.py
@@ -10,7 +10,7 @@ load_dotenv()
 api_key = st.secrets.get("OPENAI_KEY")
 st.write("key:", st.secrets["OPENAI_KEY"])
 if not api_key:
-    st.error("OpenAI API key is missing!" + api_key)
+    st.error("OpenAI API key is missing!")
     st.stop()
 # open_ai_key = st.secrets["OPENAI_KEY"]["key"]
 # gpt_key = os.getenv('OPENAI_KEY')
