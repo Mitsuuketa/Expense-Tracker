3f46e1e6a7bdcc7622b3bbb7a34e6790afbfb104
dycanee <kendycane13@gmail.com>
2025-11-29 15:31:03 +0800

    deployed

--- interactive_tracker.py
+++ interactive_tracker.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 api_key = st.secrets.get("OPENAI_KEY")
-st.write("key:", st.secrets["OPENAI_KEY"])
 if not api_key:
     st.error("OpenAI API key is missing!")
     st.stop()
